7808c6b2b902cc354e3ccf8da2342b267dde1dbd
nirvan <nirvanyamil@gmail.com>
2015-12-24 10:35:16 -0800

    agrege litio

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 var data = {
 	hidrogeno: 'n=1 l=0 m=0 s=+1/2',
-	helio: 'n=1 l=1 m=1 s=-1/2'
+	helio: 'n=1 l=1 m=1 s=-1/2',
+	litio: 'n=2 l=0 m=0 s=+1/2'
 };
 
 function getNumerosCuanticos(element) {
