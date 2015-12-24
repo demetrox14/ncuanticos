f00fb74e7b035e068af7e4f8efe75f53905d3b80
Jaime <info@mintitmedia.com>
2015-12-24 10:11:07 -0800

    init

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+var data = {
+	hidrogeno: 'n=1 l=0 m=0 s=+1/2',
+	helio: 'n=1 l=1 m=1 s=-1/2'
+};
+
+function getNumerosCuanticos(element) {
+	console.log('getNumerosCuanticos', element);
+	$('#results').html(data[element]);
+}
